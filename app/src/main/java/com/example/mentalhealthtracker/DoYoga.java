00b1ef05dc7c44d99bb901c10afd89b9b140e668
Ben Agnes <bagnes@sfu.ca>
2020-11-02 22:25:17 -0800

    implement welcome message and two working component panels
    Signed-off-by: Ben Agnes <bagnes@sfu.ca>

---
+++ app/src/main/java/com/example/mentalhealthtracker/DoYoga.java
@@ -0,0 +1,14 @@
+package com.example.mentalhealthtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DoYoga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_do_yoga);
+    }
+}
--- app/src/main/java/com/example/mentalhealthtracker/MainActivity.java
+++ app/src/main/java/com/example/mentalhealthtracker/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.mentalhealthtracker;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -12,9 +15,19 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private Button doYogaButton;
+    private Button playMusicButton;
+    private TextView welcomeMsg;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +43,51 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // set welcome message based on time of day
+        welcomeMsg = (TextView) findViewById(R.id.welcomeMsg);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH");
+        LocalDateTime currentTime = LocalDateTime.now();
+        String hourofDay = currentTime.format(formatter);
+        int hour = Integer.parseInt(hourofDay);
+        if (hour >= 4 && hour < 12 ) {
+            welcomeMsg.setText("Good morning!");
+        }
+        else if (hour >= 12 && hour < 18) {
+            welcomeMsg.setText("Good afternoon!");
+        }
+        else {
+            welcomeMsg.setText("Good evening!");
+        }
+
+        // Button that links to 'Do Yoga' component
+        doYogaButton = (Button) findViewById(R.id.yogaButton);
+        doYogaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openYogaActivity();
+            }
+        });
+
+        // Button that links to 'Play Music' component
+        playMusicButton = (Button) findViewById(R.id.playMusicButton);
+        playMusicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPlayMusicActivity();
+            }
+        });
+
+    }
+
+    public void openYogaActivity() {
+        Intent intent = new Intent(this, DoYoga.class);
+        startActivity(intent);
+    }
+
+    public void openPlayMusicActivity() {
+        Intent intent = new Intent(this, PlayMusic.class);
+        startActivity(intent);
     }
 
     @Override
