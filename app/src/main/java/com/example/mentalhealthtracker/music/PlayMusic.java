a69b375d9acc4f2a83a00c59c876bfb6bcd170fa
Ben Agnes <bagnes@sfu.ca>
2020-11-12 21:28:53 -0800

    Implement Favourite Resources
    Signed-off-by: Ben Agnes <bagnes@sfu.ca>

--- app/src/main/java/com/example/mentalhealthtracker/music/PlayMusic.java
+++ app/src/main/java/com/example/mentalhealthtracker/music/PlayMusic.java
@@ -445,6 +445,7 @@ public class PlayMusic extends AppCompatActivity {
             return;
         }
 
+        musicPlayer.reset();
         musicPlayer.release();
         musicPlayer = null;
         handler.removeCallbacks(updateUIThread);
